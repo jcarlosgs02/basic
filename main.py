2f697f0ada2db06f4602c74d2c411809529911bf
CarlosGarcia86 <89940978+jcarlosgs02@users.noreply.github.com>
2021-09-20 16:41:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+a=12
+print(a)
+print(type(a))
+b='good day'
+print(b)
+print(type(b))
+
+fval=-1.4e-3
+print(fval)
+
+a=22.35e1
+b=int(a)
+c=float(b)
+
+print(a)
+print(b)
+print(c)
+
+x='hello'
+x='goodbye'
+print('hello',x)
+x=90
+z='hello'
+print(x,z)
